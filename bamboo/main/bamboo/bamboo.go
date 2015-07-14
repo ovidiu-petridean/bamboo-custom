b2aaaf346403e4b0db99e1a02ddd06bc3ed1fc0a
Ovidiu Petridean <opetridean@sdl.com>
2015-07-14 15:23:49 +0300

    reverted config file path

--- bamboo/main/bamboo/bamboo.go
+++ bamboo/main/bamboo/bamboo.go
@@ -31,7 +31,8 @@ var logPath string
 var serverBindPort string
 
 func init() {
-	flag.StringVar(&configFilePath, "config", "/home/opetridean/go/src/github.com/QubitProducts/bamboo/config/development.json", "Full path of the configuration JSON file")
+//	flag.StringVar(&configFilePath, "config", "/home/opetridean/go/src/github.com/QubitProducts/bamboo/config/development.json", "Full path of the configuration JSON file")
+	flag.StringVar(&configFilePath, "config", "config/development.json", "Full path of the configuration JSON file")
 	flag.StringVar(&logPath, "log", "", "Log path to a file. Default logs to stdout")
 	flag.StringVar(&serverBindPort, "bind", ":8000", "Bind HTTP server to a specific port")
 }
